b27461a5cc43e9489f9e891c4ab6d0099e18c3ec
Fuji, Goro (gfx) <gfuji@cpan.org>
2014-05-05 14:14:10 +0900

    add tests to example app

--- example/src/androidTest/java/AssertTest.java
+++ example/src/androidTest/java/AssertTest.java
@@ -1,9 +1,24 @@
 import android.test.AndroidTestCase;
 
+import com.github.gfx.debugassert.example.Foo;
+
 public class AssertTest extends AndroidTestCase {
     public void testAssert() throws Exception {
-        int num = 10;
-        assert num == 20;
+        try {
+            int num = 10;
+            assert num == 20;
+            fail("must not be reached");
+        } catch (AssertionError e) {
+            assertNotNull(e);
+        }
+    }
 
+    public void testAssertEnabledInAppClasses() throws Exception {
+        try {
+            Foo.f(false);
+            fail("must not be reached");
+        } catch (AssertionError e) {
+            assertNotNull(e);
+        }
     }
 }
